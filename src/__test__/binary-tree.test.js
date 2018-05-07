f87da8a3c1336456e3f48465bc1cb4df87f1227c
zachary schumpert <buphnezz@gmail.com>
2018-05-06 20:13:55 -0700

    lab 15, needs more work

---
+++ src/__test__/binary-tree.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import BinaryTree from '../lib/binary-tree';
+
+describe('traversals.js', () => {
+  test('should return array with values in correct order', () => {
+    expect(BinaryTree(1)).toEqual(1);
+  });
+});
